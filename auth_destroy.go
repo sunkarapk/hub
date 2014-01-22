a9372c701eea7eba5a38c09c8d6928f7355cdaa8
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-01-22 12:52:23 +0530

    Finished auth_destroy

--- auth_destroy.go
+++ auth_destroy.go
@@ -3,5 +3,10 @@ package main
 type AuthDestroyCommand struct{}
 
 func (a *AuthDestroyCommand) Execute(args []string) error {
+	Config("user", "")
+	Config("token", "")
+
+	HandleInfo("You are now in `public` mode")
+
 	return nil
 }
