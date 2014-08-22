314bc78e4c9c25bb4cf26f9969c0211bdd072206
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-08-23 02:32:14 +0530

    Completed fetch command

--- fetch.go
+++ fetch.go
@@ -1,21 +1,48 @@
 package main
 
 import (
-	"errors"
+	"strings"
 )
 
-type FetchCommand struct {
-	Private bool `short:"p" long:"private" description:"Use private url for the repository"`
-}
+type FetchCommand struct{}
 
 func (f *FetchCommand) Execute(args []string) error {
 	if len(args) == 0 {
-		return errors.New("Please specify user(s)!")
+		return &ErrArgument{}
+	} else if len(args) > 1 {
+		return &ErrProxy{}
 	}
+
+	if len(strings.Split(args[0], "/")) != 1 {
+		return &ErrProxy{}
+	}
+
+	users := strings.Split(args[0], ",")
+	remotes, err := Remotes()
+
+	remoteAdd := &RemoteAddCommand{}
+
+	if err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		if _, ok := remotes[user]; !ok {
+			remoteAdd.Execute([]string{user})
+		}
+	}
+
+	err = Git([]string{"fetch", "--multiple", strings.Join(users, " ")}...)
+
+	if err != nil {
+		return err
+	}
+
+	HandleInfo("Fetched from remotes " + args[0])
 
 	return nil
 }
 
 func (f *FetchCommand) Usage() string {
-	return "[-p] <user | users>"
+	return "<user | users>"
 }
